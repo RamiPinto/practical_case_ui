0f7e23d46ba7d24dd66606a2e4c31cd670bc3d5e
RamiPinto <ramiropintoprieto@gmail.com>
2017-12-11 20:04:38 +0100

    SQL script debugged

--- src/for creating table.sql
+++ src/for creating table.sql
@@ -1,38 +1,38 @@
-﻿CREATE TABLE users (
+CREATE OR REPLACE TABLE users (
    id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-   nickname text NOT NULL, 
+   nickname text NOT NULL,
    login text NOT NULL,
    password text NOT NULL,
    photopath text NOT NULL
 );
 
-CREATE TABLE comment 
-( idComment int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, 
-nickname text NOT NULL, 
-date date NOT NULL, 
-message text NOT NULL, 
-idVideo int(11) NOT NULL 
-)
+CREATE OR REPLACE TABLE comment
+( idComment int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+nickname text NOT NULL,
+date date NOT NULL,
+message text NOT NULL,
+idVideo int(11) NOT NULL
+);
 
-CREATE TABLE playlists ( 
-    idPlaylists int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, 
-    title text NOT NULL, 
-    amVid int(11) NOT NULL, 
-    type text NOT NULL, 
-    idUser int(11) NOT NULL 
-)
+CREATE OR REPLACE TABLE playlists (
+    idPlaylists int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    title text NOT NULL,
+    amVid int(11) NOT NULL,
+    type text NOT NULL,
+    idUser int(11) NOT NULL
+);
 
-CREATE TABLE videos ( 
-    idVideos int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, 
-    path text NOT NULL, 
-    title text NOT NULL
-    likes int(11) NOT NULL, 
-    dislikes int(11) NOT NULL, 
+CREATE OR REPLACE TABLE videos (
+    idVideos int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    path text NOT NULL,
+    title text NOT NULL,
+    likes int(11) NOT NULL,
+    dislikes int(11) NOT NULL,
     views int(11) NOT NULL,
     description text NOT NULL
-)
-INSERT INTO `videos`(`idVideos`, `path`,`title`, `likes`, `dislikes`, `views`, `description`) VALUES 
-(NULL,'https://www.youtube.com/embed/qi2m4V21bw4','Cooking Spaghetti','0','0','0','Mr Bean tries to cook spaghetti for Irma but it will not fit in the pan. He tries to boil it in his bath instead. From animated episode Dinner for Two.'), 
+);
+INSERT INTO `videos`(`idVideos`, `path`,`title`, `likes`, `dislikes`, `views`, `description`) VALUES
+(NULL,'https://www.youtube.com/embed/qi2m4V21bw4','Cooking Spaghetti','0','0','0','Mr Bean tries to cook spaghetti for Irma but it will not fit in the pan. He tries to boil it in his bath instead. From animated episode Dinner for Two.'),
 (NULL,'https://www.youtube.com/embed/tZ-CGpN1wyk','Penguins of Madagascar','0','0','0','Official opening minutes from The Penguins of Madagascar'),
 (NULL,'https://www.youtube.com/embed/ahp9vqeLCd4','Behind the Scenes With The Cast','0','0','0','Mr Bean tries to cook spaghetti for Irma but it will not fit in the pan. He tries to boil it in his bath instead. From animated episode Dinner for Two.'),
 (NULL,'https://www.youtube.com/embed/twU2WaVPDdY','Helga Has Self-Respect | Hey Arnold! | The Splat','0','0','0','The Splat is your late-night destination for your favorite childhood Nickelodeon cartoons and live-action shows. We don’t question football-shaped heads, but embrace them – along with Reptar bars, a Big Ear of Corn, orange soda, and even slime for Pete (and Pete’s) sake.'),
@@ -43,10 +43,10 @@ INSERT INTO `videos`(`idVideos`, `path`,`title`, `likes`, `dislikes`, `views`, `
 (NULL,'https://www.youtube.com/embed/ZSdrf5wGNzI','SpongeBob SquarePants | Krabby Patty Contest | Nickelodeon UK','0','0','0','It is the Krabby Patty contest, the Visitors vs. the Krusty Krab, whoever can eat the most Krabby Pattys is the winner!'),
 (NULL,'https://www.youtube.com/embed/HbSQbzwQYjg','SpongeBob SquarePants | Kenny the Cat | Nickelodeon UK','0','0','0','SpongeBob and Patrick spend all night waiting for the legendary Kenny the Cat to arrive in Bikini Bottom!  When Kenny finally arrives, SpongeBob invites him for a signature Krabby Patty at the Krusty Krab... Only to find out Kennys secret!'),
 (NULL,'https://www.youtube.com/embed/cXQYyE3DQGY','SpongeBob SquarePants','0','0','0','Welcome to the official Nickelodeon UK YouTube channel! We share sneak-peeks, behind the scenes gossip and hilarious videos from all of your favourite Nickelodeon shows!'),
-(NULL,'https://www.youtube.com/embed/RoFglgMdaeE','Mr Bean: cartoon "Artful Bean"','0','0','0','Mr Bean cartoon/animation  Complete Collection Like me on Facebook Friends!  For free new movies!')
+(NULL,'https://www.youtube.com/embed/RoFglgMdaeE','Mr Bean: cartoon "Artful Bean"','0','0','0','Mr Bean cartoon/animation  Complete Collection Like me on Facebook Friends!  For free new movies!');
 
-INSERT INTO `videos`(`idVideos`, `path`,`title`, `likes`, `dislikes`, `views`, `description`) VALUES 
-(NULL,'../img/home_pictures/1.png','Cooking Spaghetti','0','0','0','Mr Bean tries to cook spaghetti for Irma but it will not fit in the pan. He tries to boil it in his bath instead. From animated episode Dinner for Two.'), 
+INSERT INTO `videos`(`idVideos`, `path`,`title`, `likes`, `dislikes`, `views`, `description`) VALUES
+(NULL,'../img/home_pictures/1.png','Cooking Spaghetti','0','0','0','Mr Bean tries to cook spaghetti for Irma but it will not fit in the pan. He tries to boil it in his bath instead. From animated episode Dinner for Two.'),
 (NULL,'../img/home_pictures/2.png','Penguins of Madagascar','0','0','0','Official opening minutes from The Penguins of Madagascar'),
 (NULL,'../img/home_pictures/3.png','Behind the Scenes With The Cast','0','0','0','Mr Bean tries to cook spaghetti for Irma but it will not fit in the pan. He tries to boil it in his bath instead. From animated episode Dinner for Two.'),
 (NULL,'../img/home_pictures/4.png','Helga Has Self-Respect | Hey Arnold! | The Splat','0','0','0','The Splat is your late-night destination for your favorite childhood Nickelodeon cartoons and live-action shows. We don’t question football-shaped heads, but embrace them – along with Reptar bars, a Big Ear of Corn, orange soda, and even slime for Pete (and Pete’s) sake.'),
@@ -57,16 +57,16 @@ INSERT INTO `videos`(`idVideos`, `path`,`title`, `likes`, `dislikes`, `views`, `
 (NULL,'../img/home_pictures/9.png','SpongeBob SquarePants | Krabby Patty Contest | Nickelodeon UK','0','0','0','It is the Krabby Patty contest, the Visitors vs. the Krusty Krab, whoever can eat the most Krabby Pattys is the winner!'),
 (NULL,'../img/home_pictures/10.png','SpongeBob SquarePants | Kenny the Cat | Nickelodeon UK','0','0','0','SpongeBob and Patrick spend all night waiting for the legendary Kenny the Cat to arrive in Bikini Bottom!  When Kenny finally arrives, SpongeBob invites him for a signature Krabby Patty at the Krusty Krab... Only to find out Kennys secret!'),
 (NULL,'../img/home_pictures/11.png','SpongeBob SquarePants','0','0','0','Welcome to the official Nickelodeon UK YouTube channel! We share sneak-peeks, behind the scenes gossip and hilarious videos from all of your favourite Nickelodeon shows!'),
-(NULL,'../img/home_pictures/12.png','Mr Bean: cartoon "Artful Bean"','0','0','0','Mr Bean cartoon/animation  Complete Collection Like me on Facebook Friends!  For free new movies!')
+(NULL,'../img/home_pictures/12.png','Mr Bean: cartoon "Artful Bean"','0','0','0','Mr Bean cartoon/animation  Complete Collection Like me on Facebook Friends!  For free new movies!');
 
-CREATE TABLE favLists ( 
-    id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, 
-    idUser int(11) NOT NULL, 
-    idPlaylist isnt(11) NOT NULL
-)
-
-CREATE TABLE favVid ( 
-    id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, 
-    idVideo int(11) NOT NULL, 
+CREATE OR REPLACE TABLE favLists (
+    id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    idUser int(11) NOT NULL,
     idPlaylist int(11) NOT NULL
-)
+);
+
+CREATE OR REPLACE TABLE favVid (
+    id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    idVideo int(11) NOT NULL,
+    idPlaylist int(11) NOT NULL
+);
